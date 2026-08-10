3b1e83ccf5b19974cad01f170541ca73559b62e0
Caio Carvalho <caiocarvalho.py@gmail.com>
2026-08-09 21:38:35 -0400

    Fix import order in jacobian tests

--- tests/oracle/test_jacobian.py
+++ tests/oracle/test_jacobian.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pytest
 import torch
-from cotton_math_lab.autodiff.jacobian import jacobian
 
+from cotton_math_lab.autodiff.jacobian import jacobian
 from cotton_math_lab.autodiff.tensor import Tensor
 
 
